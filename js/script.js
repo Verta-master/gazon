f03a4292cc3ba5c43fbb504eabfc010400c8528b
Verta-master <vertyagina@gmail.com>
2019-01-16 18:42:31 +0600

    Header

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ jQuery(function($) {
 $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
   $('.menu__list').slideToggle();
-})
+});
+  
+//Header scroll
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $(".header-internal").addClass('header-internal--scroll');
+  } else {
+    $(".header-internal").removeClass('header-internal--scroll');
+  };
+});
 
 });
